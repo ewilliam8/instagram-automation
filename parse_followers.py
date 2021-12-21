5262f598fbca6a5171171b4436b838e352c93845
Ilya <rogachev.ilya@list.ru>
2021-12-21 18:42:44 +0700

    first commit

--- parse_followers.py
+++ parse_followers.py
@@ -1,16 +1,19 @@
-from config  import insta_username,\
-                    insta_password
+import os
+from config import *
 from instapy import InstaPy
 from instapy import smart_run
-import os
+from dotenv import load_dotenv
 
-session = InstaPy(username=insta_username,
-                  password=insta_password,
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+session = InstaPy(username=os.getenv("INSTA_USERNAME"),
+                  password=os.getenv("INSTA_PASSWORD"),
                   headless_browser=False,
                   bypass_security_challenge_using='sms')
 
-PARSE_FOLDER  = "PARSE/"
-target_accounts = ["skycode_school"]
+target_accounts = [account]
 
 if not os.path.isdir(PARSE_FOLDER):
     os.mkdir(PARSE_FOLDER)
@@ -18,19 +21,12 @@ if not os.path.isdir(PARSE_FOLDER):
 with smart_run(session):
 
     for account in target_accounts:
-    
         f = open(PARSE_FOLDER + account + '_followers.txt', 'w')
-        target_followers = session.grab_followers(username=account, amount="full", live_match=False, store_locally=False)
+        target_followers = session.grab_followers(username=account,
+                                                  amount="full",
+                                                  live_match=False,
+                                                  store_locally=False)
         for el in target_followers:
             f.write(el + "\n")
 
         f.close()
-
-
-        
-
-
-    
-
-
-
